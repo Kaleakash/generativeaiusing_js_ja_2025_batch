6b38acf94901bab36f0e6528af308e4fa6a90f9c
Akash <akash300383@gmail.com>
2025-01-30 16:09:56 +0530

    done

---
+++ Programs/External JS file/pre-defined-fun.js
@@ -0,0 +1,12 @@
+//alert("Welcome to external JS file");
+
+// var fname = prompt("Enter your name");
+// alert("your name is "+fname);
+do{
+var a=prompt("Enter the value of a")
+var b=prompt("Enter the value of b")
+var sum  = eval(a)+eval(b);
+alert("sum of a and b is "+sum);
+}while(confirm("Do you want to continue?"));
+//alert("Welcome to external JS file");
+alert("Thank you for using our website");
---
+++ Programs/External JS file/welcome.js
@@ -0,0 +1,3 @@
+document.writeln("Welcome to our website! using external JS file<br/>");
+document.writeln("Welcome to our website! using external JS file<br/>");
+//alert("Hello! I am an alert box!!");
