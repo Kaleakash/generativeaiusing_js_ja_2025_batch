8a8a8890ae4f3190254fa9fc6d3f5823f0ad3955
Akash <akash300383@gmail.com>
2025-02-04 16:00:31 +0530

    done

--- Programs/node js basic program/server.js
+++ Programs/node js basic program/server.js
@@ -1,10 +1,16 @@
 var http = require('http');
-
+var url = require('url');
 
 http.createServer(function (req, res) {
-
+var pathname = url.parse(req.url).pathname;
+console.log("Request for " + pathname + " received.");
+console.log(req.method)
+console.log(req.headers)
+var query = url.parse(req.url, true).query;
+console.log(query)
+console.log(req.url)
 //res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
+res.end('Welcome user ' + query.name + ' to the server');
 
 
 
